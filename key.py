fcb579a618763b4df65b13626db8cd8615264801
MSnaker <55047570+MSnaker@users.noreply.github.com>
2021-09-21 16:29:10 +0200

    main.py
    Further progress in the main.py script

--- key.py
+++ key.py
@@ -4,24 +4,23 @@ from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+# Generate key, start fernet.
 with open('salt.txt','rb') as fsalt:
     salt = fsalt.readlines()[0]
 pwd = b'banananas'
-
 kdf = PBKDF2HMAC(
     algorithm=hashes.SHA256(),
     length=32,  
     salt=salt,
     iterations=420069,
 )
-key = base64.urlsafe_b64encode(kdf.derive(password))
-
+key = base64.urlsafe_b64encode(kdf.derive(pwd))
 f = password.start_Fernet(salt, pwd)
 
 proof = b'Bananarama was pretty good!'
 enc_proof = f.encrypt(proof)
 
-with open('./text/key.txt','wb') as fkey:
+with open('key.txt','wb') as fkey:
     fkey.write(key)
     fkey.write(b'\n')
     fkey.write(proof)
--- main.py
+++ main.py
@@ -5,21 +5,25 @@ with open('salt.txt','rb') as fsalt:
     salt = fsalt.readlines()[0]
 
 arr_lines = password.import_encrypted()
+enc_proof = b'gAAAAABhSd17y7PzMpGyLmQD7TZlPbXkURzrEmuO2SADY1Lc-HkQgNkoPPl6iqANnriaZNOWanv5B2rSj0M-iFiqwsxyuntKw_rdd1xb8ErZLp09QlvRAgY='
 
 def press(button):
     if button == 'Cancel':
         app.stop()
     if button == 'Acquire':
-        password = app.getEntry('Password')
-        f = password.start_Fernet(salt,password)
-        
-
-
-
+        pwd = app.getEntry('PwdEnt')
+        try:
+            #code here to create key from a password
+            f = password.start_Fernet(salt,pwd)
+            proof=str(f.decrypt(enc_proof),'utf-8')
+        except (cryptography.fernet.InvalidToken, TypeError):
+            proof = "wow, much try"
+        app.setLabel('proofLabel', proof)
 
 
 app = gui()
+app.addLabel('proofLabel',"Proof label")
+app.addEntry('PwdEnt')
 
-app.addLabel("title", "Welcome to appJar")
 
-app.go(startWindow==None)
+app.go()
