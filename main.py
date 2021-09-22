3fd1629bbd51427d7933d92a51cd832e21d30245
MSnaker <55047570+MSnaker@users.noreply.github.com>
2021-09-22 09:38:05 +0200

    Modified password.py, creating the Password class. Implemented in main.py
    The Password class contains the methods used within the main script, and the main method now returns a functional GUI, which also returns a confirmation message to let you know you put in the correct password.

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
-import password
+import os
+from password import Password
 from appJar import gui
+import cryptography
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-with open('salt.txt','rb') as fsalt:
+print(os.getcwd())
+with open('./text/salt.txt','rb') as fsalt:
     salt = fsalt.readlines()[0]
-
-arr_lines = password.import_encrypted()
+print(os.getcwd())
+arr_lines = Password.import_encrypted()
 enc_proof = b'gAAAAABhSd17y7PzMpGyLmQD7TZlPbXkURzrEmuO2SADY1Lc-HkQgNkoPPl6iqANnriaZNOWanv5B2rSj0M-iFiqwsxyuntKw_rdd1xb8ErZLp09QlvRAgY='
 
 def press(button):
     if button == 'Cancel':
         app.stop()
+    pwd = bytes(app.getEntry('PwdEnt'),'utf-8')
+    try:
+        #code here to create key from a password
+        f = Password.start_Fernet(salt,pwd)
+        proof=str(f.decrypt(enc_proof),'utf-8')
+    except (cryptography.fernet.InvalidToken, TypeError):
+        proof = "wow, much try"
     if button == 'Acquire':
-        pwd = app.getEntry('PwdEnt')
-        try:
-            #code here to create key from a password
-            f = password.start_Fernet(salt,pwd)
-            proof=str(f.decrypt(enc_proof),'utf-8')
-        except (cryptography.fernet.InvalidToken, TypeError):
-            proof = "wow, much try"
-        app.setLabel('proofLabel', proof)
+        app.setLabel('proofLab', proof)
 
 
-app = gui()
-app.addLabel('proofLabel',"Proof label")
-app.addEntry('PwdEnt')
+with gui('Password Djinnerator') as app:
+    app.addLabel('proofLab','',row = 1, colspan=2)    
+    app.addLabel('pwdLab', 'Enter Password', row=0,column=0)
+    app.addEntry('PwdEnt',row = 0, column = 1)
 
+    app.addButtons(['Cancel', 'Acquire'], press, row = 2)
 
 app.go()
--- password.py
+++ password.py
@@ -1,4 +1,4 @@
-'''This is a script that takes the previously encoded file, '''
+'''This file initialises the class Password, which contains all the methods necessary to run the main.py script.'''
 
 import os
 import numpy as np
@@ -9,106 +9,98 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from numpy.core.arrayprint import printoptions
 from numpy.core.records import array
 
-# Function to import all pages of the origin file as a list (of lists) [list_pages(list_lines)].
-def import_encrypted():
-    print('Importing all lines from the encoded file...')
-    os.chdir('./text')
-    with open('encoded.txt','rb') as fr:
-        list_lines = fr.readlines()
+class Password():
+    def import_encrypted():
+        '''Function to import all pages of the origin file as a list (of lists) [list_pages(list_lines)].'''
+        
+        print('Importing all lines from the encoded file...')
+        os.chdir('./text')
+        with open('encoded.txt','rb') as fr:
+            list_lines = fr.readlines()
 
-    print('Structuring the data...')
-    w, h = 38,31
-    arr_lines = [['boba' for x in range(w)] for y in range(h)]
-    page, i = 0, 0
-    for line in list_lines:
-        if line.startswith(b'Page'):
-            i = 0
-            if len(line)>7:
-                page = 10*int(chr(line[5]))+int(chr(line[6])) 
-            else:
-                page = int(chr(line[5]))
-            print('Accessing page', page,'...')
-            continue
-        arr_lines[page][i] = line
-        i+=1
+        print('Structuring the data...')
+        w, h = 38,31
+        arr_lines = [['boba' for x in range(w)] for y in range(h)]
+        page, i = 0, 0
+        for line in list_lines:
+            if line.startswith(b'Page'):
+                i = 0
+                if len(line)>7:
+                    page = 10*int(chr(line[5]))+int(chr(line[6])) 
+                else:
+                    page = int(chr(line[5]))
+                print('Accessing page', page,'...')
+                continue
+            arr_lines[page][i] = line
+            i+=1
 
-    i = 0
-    del arr_lines[0]
-    for page in arr_lines:
-        i += 1
-        for j in range(len(page)):
-            if page[-1] == 'boba':
-                del page[-1]
-    return arr_lines
+        i = 0
+        del arr_lines[0]
+        for page in arr_lines:
+            i += 1
+            for j in range(len(page)):
+                if page[-1] == 'boba':
+                    del page[-1]
+        return arr_lines
 
-arr_lines = import_encrypted()
+    def start_Fernet(salt,password):
+        '''This function is to set up the function Fernet to decode the lines.'''
 
-# This section is to use the same password and salt used before
-print('Acquiring password...')
-password = b'banananas'
-print('Obtaining salt...')
-with open('salt.txt','rb') as fsalt:
-    salt = fsalt.readlines()[0]
+        print('Initialising Fernet...')
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,  
+            salt=salt,
+            iterations=420069,
+        )
+        key = base64.urlsafe_b64encode(kdf.derive(password))
+        # print('Fernet key is:', key)
+        f = Fernet(key)
+        return f
 
-# This function is to set up the function Fernet to decode the lines.
-def start_Fernet(salt,password):
-    print('Initialising Fernet...')
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,  
-        salt=salt,
-        iterations=420069,
-    )
-    key = base64.urlsafe_b64encode(kdf.derive(password))
-    # print('Fernet key is:', key)
-    f = Fernet(key)
-    return f
+    # def export_decrypted():
+        '''Writing the decoded text to another file, to prove that we are on the right path (we can encrypt and then decrypt strings)
+        We don't run this any more, as the script already gave a valid output once.'''
 
-f = start_Fernet(salt,password)
-    
-# Writing the decoded text to another file, to prove that we are on the right path (we can encrypt and then decrypt strings)
-# We don't run this any more, as the script already gave a valid output once.
-def export_decrypted():
-    i = 1
-    print('Writing to text file...')
-    with open('./decoded.txt', 'w', encoding='utf-8') as fwrite:
-        for array_page in arr_lines:
-            fwrite.write(''.join(['Page',str(i)]))
-            fwrite.write('\n')
-            i +=1
-            for bytes_line in array_page:
-                decr_line = f.decrypt(bytes_line)
-                fwrite.write(str(decr_line))
+        i = 1
+        print('Writing to text file...')
+        with open('./decoded.txt', 'w', encoding='utf-8') as fwrite:
+            for array_page in arr_lines:
+                fwrite.write(''.join(['Page',str(i)]))
                 fwrite.write('\n')
+                i +=1
+                for bytes_line in array_page:
+                    decr_line = f.decrypt(bytes_line)
+                    fwrite.write(str(decr_line))
+                    fwrite.write('\n')
 
-# This function gets the y line of the x page, and decrypts it
-def getline(arr_lines, fernet, x, y):
+    def getline(arr_lines, fernet, x, y):
+        '''This method gets the y line of the x page, and decrypts it'''
 
-    print('Decrypting', x, 'page,', y, 'line...')
-    line = arr_lines[x][y]
-    decr_line = fernet.decrypt(line)
-    return decr_line
+        print('Decrypting', x, 'page,', y, 'line...')
+        line = arr_lines[x][y]
+        decr_line = fernet.decrypt(line)
+        return decr_line
 
-# This function extracts the first n words from a string and transforms the result into a password, accepting a string separators, that will rotate based on the line number in the page.
-def pswgen(line, int_line, n, seps): 
-    tr_from = 'AEIOUY'
-    tr_to = '4310*<'
-    print('Generating password...')
-    line_split = str(line, 'utf-8').split(' ')
-    sep = seps[int_line%len(seps)]
-    first = line_split[0].upper()
-    trtable = first.maketrans(tr_from,tr_to)
-    first_trans = first.translate(trtable)
-    last = line_split[n-1].upper()
-    trtable = last.maketrans(tr_from,tr_to)
-    last_trans = last.translate(trtable)
-    mid = ''.join(line_split[1:n-1])
-    pswout = sep.join([first_trans,mid,last_trans])
+    def pswgen(line, int_line, n, seps): 
+        '''This method extracts the first n words from a string and transforms the result into a password, accepting a string separators, that will rotate based on the line number in the page.'''
+        tr_from = 'AEIOUY'
+        tr_to = '4310*<'
+        print('Generating password...')
+        line_split = str(line, 'utf-8').split(' ')
+        sep = seps[int_line%len(seps)]
+        first = line_split[0].upper()
+        trtable = first.maketrans(tr_from,tr_to)
+        first_trans = first.translate(trtable)
+        last = line_split[n-1].upper()
+        trtable = last.maketrans(tr_from,tr_to)
+        last_trans = last.translate(trtable)
+        mid = ''.join(line_split[1:n-1])
+        pswout = sep.join([first_trans,mid,last_trans])
 
-    return pswout
-
-psw  = pswgen(getline(arr_lines, f, 0, 1), 1, 3, '.,-')
+        return pswout
 
 
 
-     
+
+        
